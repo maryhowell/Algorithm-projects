cc9145e21cfaaf7656c446ae09568da60a78df49
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-08-17 14:54:40 -0700

    solution to interview problem

--- Heaps/heaps_solutions/500_files.rb
+++ Heaps/heaps_solutions/500_files.rb
@@ -9,5 +9,41 @@
 require_relative "heap"
 
 def five_hundred_files(arr_of_arrs)
+  # arr_of_arrs.concat.heap_sort! n(logn) << naive solution bad time complexity
+  # storage = []
+  #
+  # arr_of_arrs.each do |array|
+  #   storage << array[0]
+  # end
+  # storage.heap_sort!
 
+  prc = Proc.new { |el1, el2| el1[0] <=> el2[0] }
+  heap = BinaryMinHeap.new(&prc)
+  result = []
+
+  arr_of_arrs.length.times do |i|
+    heap.push([arr_of_arrs[i][0], i ,0])
+  end
+
+  while heap.count > 0
+    min = heap.extract
+    result << min[0]
+
+    next_arr_i = min[1]
+    next_idx = min[2] + 1
+    next_el = (arr_of_arrs[next_arr_i, next_idx]) if next_el
+  end
+  result
 end
+
+arr_of_arrs = [[3,5,7],[0,6], [0,6,28]]
+
+p five_hundred_files(arr_of_arrs)
+
+  # remove timestamps from arrays
+  # take first item in first arr this is the smallest item (root?)
+  # concat together
+  # heap_sort!
+  # return new arr_of_arrs
+  # storage
+# end
