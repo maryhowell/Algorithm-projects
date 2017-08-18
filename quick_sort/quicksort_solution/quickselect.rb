b485b4fa3bbe8f4c016d67a3be73d3b34f971cd6
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-08-18 15:45:13 -0700

    adding interview question of quickselect

--- quick_sort/quicksort_solution/quickselect.rb
+++ quick_sort/quicksort_solution/quickselect.rb
@@ -1,11 +1,40 @@
+# Write an implace instance method on the Array class that will find
+# the kth smallest elemtn in O(n) time.  You will likely want to use the
+# partition method similar to the on in QuickSort.
+# NB 1st smalles is the 0-th element in a sorted array.
+
 class Array
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
 
+    pivot_idx = start
+    pivot = array[start]
+
+    ((start + 1)...(start + length)).each do |idx|
+      if prc.call(pivot, array[idx]) > 0
+        array[idx], array[pivot_idx + 1] = array[pivot_idx + 1], array[idx]
+        pivot_idx += 1
+      end
+    end
+    array[start], array[pivot_idx] = array[pivot_idx], array[start]
+    pivot_idx
   end
 
   def select_kth_smallest(k)
-  
+    left = 0
+    right = self.length - 1
+    while true
+      return self[left] if left == right
+      pivot_idx = Array.partition(self, left, right - left + 1)
+      if k - 1 == pivot_idx
+        return self[k - 1]
+      elsif k - 1 < pivot_idx
+        right = pivot_idx - 1
+      else
+        left = pivot_idx + 1
+      end
+    end
   end
 end
 
